e19c9b907579a0d200a022027b269de39fbeb077
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-27 13:41:52 +0200

    Update messageDeleteBulk.js

--- src/events/messageDeleteBulk.js
+++ src/events/messageDeleteBulk.js
@@ -7,7 +7,7 @@ module.exports = async (client, messages) => {
 
   if (messagesArray[1].channel.type === 'dm') return
 
-  client.database.query('SELECT * FROM settings WHERE id = $1', [messagesArray[1].guild.id], async (err, res) => {
+  client.database.query('SELECT * FROM settings WHERE id = $1', [messagesArray[0].guild.id], async (err, res) => {
 
     const channel = res.rows[0].channels.logs
 
@@ -22,7 +22,7 @@ module.exports = async (client, messages) => {
         .setColor(client.config.embed.color)
         .setTimestamp()
         .setTitle(messagesArray[1].language.get('LOGS').MSG_DELETE_BULK[0])
-        .setDescription(`${messagesArray[1].language.get('LOGS').MSG_DELETE_BULK[1]} ${messagesArray.length} \n${messagesArray[1].language.get('LOGS').MSG_DELETE_BULK[2]} <#${messagesArray[1].channel.id}> \n\n${messagesContent}`)
+        .setDescription(`${messagesArray[0].language.get('LOGS').MSG_DELETE_BULK[1]} ${messagesArray.length} \n${messagesArray[0].language.get('LOGS').MSG_DELETE_BULK[2]} <#${messagesArray[0].channel.id}> \n\n${messagesContent}`)
         .setFooter(client.user.username, client.user.avatarURL());
 
       return client.channels.cache.get(channel).send(embed)
@@ -31,11 +31,11 @@ module.exports = async (client, messages) => {
         .setColor(client.config.embed.color)
         .setTimestamp()
         .setTitle(messagesArray[1].language.get('LOGS').MSG_DELETE_BULK[0])
-        .setDescription(`${messagesArray[1].language.get('LOGS').MSG_DELETE_BULK[1]} ${messagesArray.length} \\n${messagesArray[1].language.get('LOGS').MSG_DELETE_BULK[2]} <#${messagesArray[1].channel.id}>`)
+        .setDescription(`${messagesArray[0].language.get('LOGS').MSG_DELETE_BULK[1]} ${messagesArray.length} \\n${messagesArray[0].language.get('LOGS').MSG_DELETE_BULK[2]} <#${messagesArray[0].channel.id}>`)
         .attachFiles([{ name: 'messageDeleteBulk.txt', attachment: Buffer.from(messagesContent, 'utf8') }])
         .setFooter(client.user.username, client.user.avatarURL());
 
-      return client.channels.cache.get(channel).send(embed)
+      return messagesArray[0].channels.cache.get(channel).send(embed)
     }
 
   })
