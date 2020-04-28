a2a18e35e2c7f5a0b1d8431d3dd92403d445138b
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-28 23:21:19 +0200

    Update emojify.js

--- src/commands/Fun/emojify.js
+++ src/commands/Fun/emojify.js
@@ -22,43 +22,45 @@ module.exports = class Emojify extends Command {
   }
 
   run (message, args) {
-      const mapping = {
-        ' ': '   ',
-        0: ':zero:',
-        1: ':one:',
-        2: ':two:',
-        3: ':three:',
-        4: ':four:',
-        5: ':five:',
-        6: ':six:',
-        7: ':seven:',
-        8: ':eight:',
-        9: ':nine:',
-        '<': ':arrow_backward:',
-        '>': ':arrow_forward:',
-        '!': ':exclamation:',
-        '?': ':question:',
-        '^': ':arrow_up_small:',
-        '+': ':heavy_plus_sign:',
-        '-': ':heavy_minus_sign:',
-        '÷': ':heavy_division_sign:',
-        '.': ':radio_button:'
+    const mapping = {
+      ' ': '   ',
+      0: ':zero:',
+      1: ':one:',
+      2: ':two:',
+      3: ':three:',
+      4: ':four:',
+      5: ':five:',
+      6: ':six:',
+      7: ':seven:',
+      8: ':eight:',
+      9: ':nine:',
+      '<': ':arrow_backward:',
+      '>': ':arrow_forward:',
+      '!': ':exclamation:',
+      '?': ':question:',
+      '^': ':arrow_up_small:',
+      '+': ':heavy_plus_sign:',
+      '-': ':heavy_minus_sign:',
+      '÷': ':heavy_division_sign:',
+      '.': ':radio_button:'
 
-      }
-      'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
-        mapping[c] = mapping[c.toUpperCase()] = `:regional_indicator_${c}:`
-      })
+    }
+    'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
+      mapping[c] = mapping[c.toUpperCase()] = `:regional_indicator_${c}:`
+    })
 
-      if (!args[0]) return message.channel.send(message.language.get('EMOJIFY_ARGS'))
+    if (!args[0]) return message.channel.send(message.language.get('EMOJIFY_ARGS'))
 
-       if (args.join(' ').length > 100) return message.channel.send(message.language.get(message.language.get('EMOJIFY_LENGTH')))
+    if (args.join(' ').match(new RegExp(/[^a-zA-Z0-9\s!?+÷^<>.]/))) return message.channel.send(message.language.get('EMOJIFY_NOT_VALID'))
 
-        return message.channel.send(
-          args.join(' ')
-            .split('')
-            .map(c => mapping[c] || c)
-            .join('')
-        )
+    if (args.join(' ').length > 100) return message.channel.send(message.language.get(message.language.get('EMOJIFY_LENGTH')))
+
+    return message.channel.send(
+      args.join(' ')
+        .split('')
+        .map(c => mapping[c] || c)
+        .join('')
+    )
 
   }
 }
