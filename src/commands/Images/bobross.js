e33b3b34fcf622619737cc0859326ed347cda14d
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-29 19:16:48 +0200

    Update bobross.js

--- src/commands/Images/bobross.js
+++ src/commands/Images/bobross.js
@@ -15,7 +15,6 @@ module.exports = class Bobross extends Command {
       aliases: [],
       permission: [],
       botpermissions: ['ATTACH_FILES'],
-      description: (language) => language.get('BOBROSS_DESC'),
       usage: (language, prefix) => language.get('BOBROSS_USAGE', prefix),
       category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
       examples: (language, prefix) => language.get('BOBROSS_EXEMPLE', prefix)
