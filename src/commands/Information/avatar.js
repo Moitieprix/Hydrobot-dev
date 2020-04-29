a95b789a74913c6c1ca1e857c25695b2aeab18af
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-29 19:10:53 +0200

    Update avatar.js

--- src/commands/Information/avatar.js
+++ src/commands/Information/avatar.js
@@ -15,7 +15,6 @@ module.exports = class Avatar extends Command {
       aliases: [],
       permission: [],
       botpermissions: ['EMBED_LINKS'],
-      description: (language) => language.get('AVATAR_DESC'),
       usage: (language, prefix) => language.get('AVATAR_USAGE', prefix),
       category: (language) => language.get('UTILS').INFORMATION_CATEGORIE,
       examples: (language, prefix) => language.get('AVATAR_EXEMPLE', prefix)
