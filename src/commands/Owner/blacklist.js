1c4ff5441f83c241ba2a320d08c7e5880672bd64
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-29 16:04:59 +0200

    Update blacklist.js

--- src/commands/Owner/blacklist.js
+++ src/commands/Owner/blacklist.js
@@ -4,7 +4,6 @@ class Blacklist extends Command {
   constructor (client) {
     super(client, {
       name: 'blacklist',
-      description: language => language.get('BLACKLIST_DESC'),
       usage: language => language.get('BLACKLIST_USAGE'),
       enabled: true,
       category: (language) => language.get('UTILS').BOTSTAFF_CATEGORIE,
