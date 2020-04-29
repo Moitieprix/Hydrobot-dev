0df4925e2c5b30aeb43fd2c716efe91b3fbf75e3
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-29 19:17:04 +0200

    Update sepia.js

--- src/commands/Images/sepia.js
+++ src/commands/Images/sepia.js
@@ -15,7 +15,6 @@ module.exports = class Sepia extends Command {
       aliases: [],
       permission: [],
       botpermissions: ['ATTACH_FILES'],
-      description: (language) => language.get('SEPIA_DESC'),
       usage: (language, prefix) => language.get('SEPIA_USAGE', prefix),
       category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
       examples: (language, prefix) => language.get('SEPIA_EXEMPLE', prefix)
