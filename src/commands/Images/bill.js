f08f42164d96dea112ede281e1d5c11b102035aa
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-29 19:15:56 +0200

    Update bill.js

--- src/commands/Images/bill.js
+++ src/commands/Images/bill.js
@@ -15,7 +15,6 @@ module.exports = class Bill extends Command {
       aliases: [],
       permission: [],
       botpermissions: ['ATTACH_FILES'],
-      description: (language) => language.get('BILL_DESC'),
       usage: (language, prefix) => language.get('BILL_USAGE', prefix),
       category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
       examples: (language, prefix) => language.get('BILL_EXEMPLE', prefix)
