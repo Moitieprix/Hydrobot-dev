5e5c01c5a3abbaf9e83a9ff0f883bfb196ee82e6
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-04-30 14:37:41 +0200

    Update antilink.js

--- src/commands/Administration/antilink.js
+++ src/commands/Administration/antilink.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const {MessageEmbed} = require('discord.js')
 const Command = require('../../../core/Command.js')
 
@@ -5,84 +7,80 @@ module.exports = class Antilink extends Command {
   constructor (client) {
     super(client, {
       name: 'antilink',
-      usage: (language, prefix) => language.get('ANTILINK_USAGE', prefix),
+      cooldown: 5,
       enabled: true,
-      category: (language) => language.get('UTILS').GUILDADMIN_CATEGORY,
+      owner: false,
+      plugin: false,
       aliases: [],
       permission: ['ADMINISTRATOR'],
       botpermissions: ['EMBED_LINKS', 'MANAGE_MESSAGES'],
-      examples: (language, prefix) => language.get('ANTILINK_EXEMPLE', prefix),
-      owner: false,
-      cooldown: 5,
-      nsfw: false,
-      plugin: false
+      usage: (language, prefix) => language.get('ANTILINK_USAGE', prefix),
+      category: (language) => language.get('UTILS').GUILDADMIN_CATEGORY,
+      examples: (language, prefix) => language.get('ANTILINK_EXAMPLE', prefix)
     })
   }
 
   async run (message, args) {
     this.client.database.query('SELECT * FROM settings WHERE id = $1', [message.guild.id], async (err, res) => {
+      if (err) return message.channel.send(message.language.get('UTILS').DATABASE_ERROR(err))
+
       const data = JSON.parse(res.rows[0].antilink[0])
 
-      if (args[0] === 'addrole') {
-        if (!message.mentions.roles.first()) {
-          message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[0])
-        } else {
-          if (data.roles.length !== 0 && data.roles.includes(message.mentions.roles.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[1])
-          } else {
-            data.roles.push(message.mentions.roles.first().id)
-            this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').ADDROLE(message.mentions.roles.first().id))
-          }
-        }
-      } else if (args[0] === 'removerole') {
-        if (!message.mentions.roles.first()) {
-          message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[0])
-        } else {
-          if (data.roles.length === 0 || !data.roles.includes(message.mentions.roles.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[2])
-          } else {
-            const pos = data.roles.indexOf(message.mentions.roles.first().id)
-            data.roles.splice(pos, 1)
-            this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').REMOVEROLE(message.mentions.roles.first().id))
-          }
-        }
-      } else if (args[0] === 'addchannel') {
-        if (!message.mentions.channels.first()) {
-          message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[0])
-        } else {
-          if (data.channels.length !== 0 && data.channels.includes(message.mentions.channels.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[1])
-          } else {
-            data.channels.push(message.mentions.channels.first())
-            this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').ADDCHANNEL(message.mentions.channels.first()))
+      const role = this.client.functions.roleFilter(message, args[1])
+      const channel = this.client.functions.channelFilter(message, args[1])
 
-          }
-        }
-      } else if (args[0] === 'removechannel') {
-        if (!message.mentions.channels.first()) {
-          message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[0])
-        } else {
-          if (data.channels.length === 0 && !data.channels.includes(message.mentions.channels.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[2])
-          } else {
-            const pos = data.channels.indexOf(message.mentions.roles.first().id)
-            data.channels.splice(pos, 1)
-            this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').REMOVECHANNEL(message.mentions.channels.first()))
-          }
-        }
-      } else {
-        const embed = new MessageEmbed()
-          .setColor(this.client.config.embed.color)
-          .setTimestamp()
-          .setTitle(message.language.get('AUTOMOD').ANTILINK[4])
-          .setDescription(message.language.get('AUTOMOD').ANTILINK[5])
-          .setFooter(this.client.user.username, this.client.user.avatarURL())
+      switch (args[0]) {
+        case 'addrole':
 
-        return message.channel.send(embed)
+          if (!role) return message.channel.send(message.language.get('ANTILINK')[0])
+          if (data.roles.length !== 0 && data.roles.includes(role)) return message.channel.send(message.language.get('ANTILINK')[1])
+
+          data.roles.push(role)
+          this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTILINK_ADDROLE', role))
+          break
+
+        case 'removerole':
+
+          if (!role) return message.channel.send(message.language.get('ANTILINK')[0])
+          if (data.roles.length === 0 || !data.roles.includes(role)) return message.channel.send(message.language.get('ANTILINK')[2])
+
+          const posRole = data.roles.indexOf(role)
+          data.roles.splice(posRole, 1)
+          this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTILINK_REMOVEROLE', role))
+          break
+
+        case 'addchannel':
+
+          if (!channel) return message.channel.send(message.language.get('ANTILINK')[3])
+          if (data.channels.length !== 0 && data.channels.includes(channel)) return message.channel.send(message.language.get('ANTILINK')[4])
+
+          data.channels.push(channel)
+          this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTILINK_ADDCHANNEL', channel))
+          break
+
+        case 'removechannel':
+          if (!channel) return message.channel.send(message.language.get('ANTILINK')[3])
+          if (data.channels.length === 0 && !data.channels.includes(channel)) return message.channel.send(message.language.get('ANTILINK')[6])
+
+          const posChannel = data.channels.indexOf(channel)
+          data.channels.splice(posChannel, 1)
+          this.client.database.query('UPDATE settings SET antilink = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTILINK_REMOVECHANNEL', channel))
+          break
+
+        default:
+          const embed = new MessageEmbed()
+            .setColor(this.client.config.embed.color)
+            .setTimestamp()
+            .setTitle(message.language.get('ANTILINK')[7])
+            .setDescription(message.language.get('ANTILINK')[8])
+            .setFooter(this.client.user.username, this.client.user.avatarURL())
+
+          message.channel.send(embed)
+          break
       }
     })
   }
