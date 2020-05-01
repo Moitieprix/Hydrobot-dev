a81cc362c2e33c42c9f72fa0c09171e275e7f240
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-01 23:31:33 +0200

    Update badwords.js

--- src/commands/Administration/badwords.js
+++ src/commands/Administration/badwords.js
@@ -5,17 +5,16 @@ module.exports = class Badwords extends Command {
   constructor (client) {
     super(client, {
       name: 'badwords',
-      usage: (language, prefix) => language.get('BADWORDS_USAGE', prefix),
       enabled: true,
-      category: (language) => language.get('UTILS').GUILDADMIN_CATEGORIE,
+      owner: false,
+      cooldown: 5,
+      plugin: false,
       aliases: [],
       permission: ['ADMINISTRATOR'],
       botpermissions: ['EMBED_LINKS', 'MANAGE_MESSAGES'],
-      examples: (language, prefix) => language.get('BADWORDS_EXEMPLE', prefix),
-      owner: false,
-      cooldown: 5,
-      nsfw: false,
-      plugin: false
+      usage: (language, prefix) => language.get('BADWORDS_USAGE', prefix),
+      category: (language) => language.get('UTILS').GUILDADMIN_CATEGORY,
+      examples: (language, prefix) => language.get('BADWORDS_EXAMPLE', prefix)
     })
   }
 
@@ -23,102 +22,135 @@ module.exports = class Badwords extends Command {
     this.client.database.query('SELECT * FROM settings WHERE id = $1', [message.guild.id], async (err, res) => {
       const data = JSON.parse(res.rows[0].badwords[0])
 
-      const embed = new MessageEmbed()
-        .setColor(this.client.config.embed.color)
-        .setTimestamp()
-        .setFooter(this.client.user.username, this.client.user.avatarURL())
+      const role = this.client.functions.roleFilter(message, args[1])
+      const channel = this.client.functions.channelFilter(message, args[1])
 
-      if (args[0] === 'addrole') {
-        if (!message.mentions.roles.first()) {
-          message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[0])
-        } else {
-          if (data.roles.length !== 0 && data.roles.includes(message.mentions.roles.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[1])
-          } else {
-            data.roles.push(message.mentions.roles.first().id)
-            this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').ADDROLE(message.mentions.roles.first().id))
-          }
-        }
-      } else if (args[0] === 'removerole') {
-        if (!message.mentions.roles.first()) {
-          message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[0])
-        } else {
-          if (data.roles.length === 0 || !data.roles.includes(message.mentions.roles.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').ROLES_ERROR[2])
-          } else {
-            const pos = data.roles.indexOf(message.mentions.roles.first().id)
-            data.roles.splice(pos, 1)
-            this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').REMOVEROLE(message.mentions.roles.first().id))
-          }
-        }
-      } else if (args[0] === 'addchannel') {
-        if (!message.mentions.channels.first()) {
-          message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[0])
-        } else {
-          if (data.channels.length !== 0 && data.channels.includes(message.mentions.channels.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[1])
-          } else {
-            data.channels.push(message.mentions.channels.first())
-            this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').ADDCHANNEL(message.mentions.channels.first()))
+      switch (args[0]) {
+        case 'addrole':
+          if (!role) return message.channel.send(message.language.get('ANTICAPS')[0])
+          if (data.roles.length !== 0 && data.roles.includes(role)) return message.channel.send(message.language.get('ANTICAPS')[1])
 
-          }
-        }
-      } else if (args[0] === 'removechannel') {
-        if (!message.mentions.channels.first()) {
-          message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[0])
-        } else {
-          if (data.channels.length === 0 && !data.channels.includes(message.mentions.channels.first().id)) {
-            message.channel.send(message.language.get('AUTOMOD').CHANNELS_ERROR[2])
-          } else {
-            const pos = data.channels.indexOf(message.mentions.roles.first().id)
-            data.channels.splice(pos, 1)
-            this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').REMOVECHANNEL(message.mentions.channels.first()))
-          }
-        }
-      } else if (args[0] === 'addword') {
-        if (!args[1]) {
-          message.channel.send(message.language.get('AUTOMOD').BADWORDS[4])
-        } else {
-          if (data.words.length !== 0 && data.words.includes(args[1])) {
-            message.channel.send(message.language.get('AUTOMOD').BADWORDS[5])
-          } else {
-            data.words.push(args[1])
-            this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').ADDWORD(args[1]))
+          data.push(role)
+          this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTICAPS_ADDROLE', role))
+          break
 
-          }
-        }
-      } else if (args[0] === 'removeword') {
-        if (!args[1]) {
-          message.channel.send(message.language.get('AUTOMOD').BADWORDS[4])
-        } else {
-          if (data.words.length === 0 || !data.words.includes(args[1])) {
-            message.channel.send(message.language.get('AUTOMOD').BADWORDS[6])
-          } else {
-            const pos = data.words.indexOf(args[1])
-            data.words.splice(pos, 1)
-            this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-            message.channel.send(message.language.get('AUTOMOD').REMOVEWORD(args[1]))
-          }
-        }
-      } else if (args[0] === 'words') {
-        if (data.words.length === 0) {
-          message.channel.send(message.language.get('AUTOMOD').BADWORDS[8])
-        } else {
-          embed.setTitle(message.language.get('AUTOMOD').BADWORDS[7])
-          embed.setDescription(data.words.length === 1 ? '`' + data.words.join('') + '`' : '`' + data.words.join('`, `') + '`')
+        case 'removerole':
+          if (!role) return message.channel.send(message.language.get('ANTICAPS')[0])
+          if (data.roles.length === 0 || !data.roles.includes(role)) return message.channel.send(message.language.get('ANTICAPS')[2])
 
-          return message.channel.send(embed)
-        }
-      } else {
-        embed.setTitle(message.language.get('AUTOMOD').BADWORDS[9])
-        embed.setDescription(message.language.get('AUTOMOD').BADWORDS[10])
+          const posRole = data.roles.indexOf(role)
+          data.roles.splice(posRole, 1)
+          this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTICAPS_REMOVEROLE', role))
+          break
 
-        return message.channel.send(embed)
+        case 'addchannel':
+          if (!channel) return message.channel.send(message.language.get('ANTICAPS')[3])
+          if (data.channels.length !== 0 && data.channels.includes(channel)) return message.channel.send(message.language.get('ANTICAPS')[4])
+
+          if (message.guild.channels.cache.get(channel).type === 'voice' || message.guild.channels.cache.get(channel).type === 'category') return message.channel.send(message.language.get('ANTICAPS')[5])
+
+          data.channels.push(channel)
+          this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTICAPS_ADDCHANNEL', channel))
+          break
+
+        case 'removechannel':
+          if (!channel) return message.channel.send(message.language.get('ANTICAPS')[3])
+          if (data.channels.length === 0 && !data.channels.includes(channel)) return message.channel.send(message.language.get('ANTICAPS')[6])
+
+          const posChannel = data.channels.indexOf(channel)
+          data.channels.splice(posChannel, 1)
+          this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send(message.language.get('ANTICAPS_REMOVECHANNEL', channel))
+          break
+
+        case 'addword':
+          if (!args[1]) return message.channel.send('mot manquant')
+          if (data.words.length !== 0 && data.words.includes(args[1])) return message.channel.send('mot deja présent')
+
+          data.words.push(channel)
+          this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send('mot ajouté')
+          break
+
+        case 'removeword':
+          if (!args[1]) return message.channel.send('mot manquant')
+          if (data.words.length === 0 && !data.words.includes(args[1])) return message.channel.send('mot non présent')
+
+          const posWord = data.words.indexOf(args[1])
+          data.words.splice(posWord, 1)
+          this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
+          message.channel.send('mot retiré')
+          break
+
+        case 'words':
+          const embedWords = new MessageEmbed()
+            .setColor(this.client.config.embed.color)
+            .setTimestamp()
+            .setTitle(message.language.get('ANTICAPS')[11])
+            .setDescription(data.words.length > 0 ? '•' + data.words.join(' \n•') : 'Aucun mot banni !')
+            .setFooter(this.client.user.username, this.client.user.avatarURL())
+
+          message.channel.send(embedWords)
+          break
+
+        case 'roles':
+          let mentionRole = []
+
+          for (const role of data.roles) {
+            if (!message.guild.roles.cache.get(role)) {
+              const pos = data.roles.indexOf(role)
+              data.roles.splice(pos, 1)
+            } else {
+              mentionRole.push(`• <@&${role}>`)
+            }
+          }
+
+          const embedRoles = new MessageEmbed()
+            .setColor(this.client.config.embed.color)
+            .setTimestamp()
+            .setTitle(message.language.get('ANTICAPS')[7])
+            .setDescription(mentionRole.length > 0 ? mentionRole.join(' \n') : message.language.get('ANTICAPS')[8])
+            .setFooter(this.client.user.username, this.client.user.avatarURL())
+
+          message.channel.send(embedRoles)
+          break
+
+        case 'channels':
+          let mentionChannel = []
+
+          for (const channel of data.channels) {
+            if (!message.guild.channels.cache.get(channel)) {
+              const pos = data.channels.indexOf(channel)
+              data.roles.splice(pos, 1)
+            } else {
+              mentionChannel.push(`• <#${channel}>`)
+            }
+          }
+
+          const embedChannel = new MessageEmbed()
+            .setColor(this.client.config.embed.color)
+            .setTimestamp()
+            .setTitle(message.language.get('ANTICAPS')[9])
+            .setDescription(mentionChannel.length > 0 ? mentionChannel.join(' \n') : message.language.get('ANTICAPS')[10])
+            .setFooter(this.client.user.username, this.client.user.avatarURL())
+
+          message.channel.send(embedChannel)
+          break
+
+        default:
+          const embed = new MessageEmbed()
+            .setColor(this.client.config.embed.color)
+            .setTimestamp()
+            .setTitle(message.language.get('ANTICAPS')[11])
+            .setDescription(message.language.get('ANTICAPS')[12])
+            .setFooter(this.client.user.username, this.client.user.avatarURL())
+
+          message.channel.send(embed)
+          break
+
       }
     })
 
