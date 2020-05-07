96ee794da7b65a1b1320d8e21e5d3f4e12b6b370
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-07 14:09:08 +0200

    Update badwords.js

--- src/commands/Administration/badwords.js
+++ src/commands/Administration/badwords.js
@@ -22,64 +22,77 @@ module.exports = class Badwords extends Command {
 
   async run (message, args) {
     this.client.database.query('SELECT * FROM settings WHERE id = $1', [message.guild.id], async (err, res) => {
+      if (err) return message.channel.send(message.language.get('UTILS').DATABASE_ERROR(err))
+
       const data = JSON.parse(res.rows[0].badwords[0])
 
-      const role = this.client.functions.roleFilter(message, args[1])
-      const channel = this.client.functions.channelFilter(message, args[1])
-
       switch (args[0]) {
-        case 'addrole':
-          if (!role) return message.channel.send(message.language.get('BADWORDS')[0])
-          if (data.roles.length !== 0 && data.roles.includes(role)) return message.channel.send(message.language.get('BADWORDS')[1])
+        case 'add-role':
+          const roleAdd = this.client.functions.roleFilter(message, args[1])
 
-          data.push(role)
+          if (!roleAdd) return message.channel.send(message.language.get('BADWORDS')[0])
+          if (data.roles.length !== 0 && data.roles.includes(roleAdd)) return message.channel.send(message.language.get('BADWORDS')[1])
+
+          if (data.roles.length === 15 && !data.premium) return message.channel.send(message.language.get('UTILS').ROLES_SIZE_PREMIUM(res.rows[0].prefix))
+
+          data.push(roleAdd)
           this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-          message.channel.send(message.language.get('ADDROLE', role))
+          message.channel.send(message.language.get('ADDROLE', roleAdd))
           break
 
-        case 'removerole':
-          if (!role) return message.channel.send(message.language.get('BADWORDS')[0])
-          if (data.roles.length === 0 || !data.roles.includes(role)) return message.channel.send(message.language.get('BADWORDS')[2])
+        case 'remove-role':
+          const roleRemove = this.client.functions.roleFilter(message, args[1])
 
-          const posRole = data.roles.indexOf(role)
+          if (!roleRemove) return message.channel.send(message.language.get('BADWORDS')[0])
+          if (data.roles.length === 0 || !data.roles.includes(roleRemove)) return message.channel.send(message.language.get('BADWORDS')[2])
+
+          const posRole = data.roles.indexOf(roleRemove)
           data.roles.splice(posRole, 1)
           this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-          message.channel.send(message.language.get('REMOVEROLE', role))
+          message.channel.send(message.language.get('REMOVEROLE', roleRemove))
           break
 
-        case 'addchannel':
-          if (!channel) return message.channel.send(message.language.get('BADWORDS')[3])
-          if (data.channels.length !== 0 && data.channels.includes(channel)) return message.channel.send(message.language.get('BADWORDS')[4])
+        case 'add-channel':
+          const channelAdd = this.client.functions.channelFilter(message, args[1])
 
-          if (message.guild.channels.cache.get(channel).type === 'voice' || message.guild.channels.cache.get(channel).type === 'category') return message.channel.send(message.language.get('BADWORDS')[5])
+          if (!channelAdd) return message.channel.send(message.language.get('BADWORDS')[3])
+          if (data.channels.length !== 0 && data.channels.includes(channelAdd)) return message.channel.send(message.language.get('BADWORDS')[4])
 
-          data.channels.push(channel)
+          if (data.roles.length === 15 && !data.premium) return message.channel.send(message.language.get('UTILS').CHANNELS_SIZE_PREMIUM(res.rows[0].prefix))
+
+          if (message.guild.channels.cache.get(channelAdd).type === 'voice' || message.guild.channels.cache.get(channelAdd).type === 'category') return message.channel.send(message.language.get('BADWORDS')[5])
+
+          data.channels.push(channelAdd)
           this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-          message.channel.send(message.language.get('ADDCHANNEL', channel))
+          message.channel.send(message.language.get('ADDCHANNEL', channelAdd))
           break
 
-        case 'removechannel':
-          if (!channel) return message.channel.send(message.language.get('BADWORDS')[3])
-          if (data.channels.length === 0 && !data.channels.includes(channel)) return message.channel.send(message.language.get('BADWORDS')[6])
+        case 'remove-channel':
+          const channelRemove = this.client.functions.channelFilter(message, args[1])
 
-          const posChannel = data.channels.indexOf(channel)
+          if (!channelRemove) return message.channel.send(message.language.get('BADWORDS')[3])
+          if (data.channels.length === 0 && !data.channels.includes(channelRemove)) return message.channel.send(message.language.get('BADWORDS')[6])
+
+          const posChannel = data.channels.indexOf(channelRemove)
           data.channels.splice(posChannel, 1)
           this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
-          message.channel.send(message.language.get('REMOVECHANNEL', channel))
+          message.channel.send(message.language.get('REMOVECHANNEL', channelRemove))
           break
 
-        case 'addword':
+        case 'add-word':
           if (!args[1]) return message.channel.send(message.language.get('BADWORDS')[7])
-          if (data.words.length !== 0 && data.words.includes(args[1])) return message.channel.send('mot deja présent')
+          if (data.words.length !== 0 && data.words.includes(args[1])) return message.channel.send(message.language.get('BADWORDS')[8])
+
+          if (data.roles.length === 15 && !data.premium) return message.channel.send(message.language.get('UTILS').WORDS_SIZE_PREMIUM(res.rows[0].prefix))
 
           data.words.push(args[1])
           this.client.database.query('UPDATE settings SET badwords = $1 WHERE id = $2', [[data], message.guild.id])
           message.channel.send(message.language.get('ADDWORD', args[1]))
           break
 
-        case 'removeword':
+        case 'remove-word':
           if (!args[1]) return message.channel.send('mot manquant')
-          if (data.words.length === 0 && !data.words.includes(args[1])) return message.channel.send('mot non présent')
+          if (data.words.length === 0 && !data.words.includes(args[1])) return message.channel.send(message.language.get('BADWORDS')[9])
 
           const posWord = data.words.indexOf(args[1])
           data.words.splice(posWord, 1)
@@ -87,11 +100,11 @@ module.exports = class Badwords extends Command {
           message.channel.send(message.language.get('REMOVEWORD', args[1]))
           break
 
-        case 'setsanction':
+        case 'set-sanction':
           const embedSanction = new MessageEmbed()
             .setColor(this.client.config.embed.color)
-            .setTitle(message.language.get('BADWORDS')[8])
-            .setDescription(message.language.get('BADWORDS')[9])
+            .setTitle(message.language.get('BADWORDS')[10])
+            .setDescription(message.language.get('BADWORDS')[11])
             .setTimestamp()
             .setFooter(this.client.user.username, this.client.user.avatarURL())
 
@@ -127,11 +140,11 @@ module.exports = class Badwords extends Command {
           const embedSetup = new MessageEmbed()
             .setColor(this.client.config.embed.color)
             .setTimestamp()
-            .setTitle(message.language.get('BADWORDS')[10])
-            .addField(message.language.get('BADWORDS')[11], message.language.get('SANCTION')[data.sanction - 1])
-            .addField(message.language.get('BADWORDS')[12], data.words.length > 0 ? '•' + data.words.join(' \n•') : message.language.get('BADWORDS')[13])
-            .addField(message.language.get('BADWORDS')[14], `${mentionRole.length > 0 ? `${mentionRole.join(' \n').length > 1000 ? `${mentionRole.slice(0, 9).join(' \n')} ${message.language.get('UTILS').MORE_SIZE(mentionRole.length - 9)}` : mentionRole.join(' \n')}` : message.language.get('ANTICAPS')[15]}`)
-            .addField(message.language.get('BADWORDS')[16], `${mentionChannel.length > 0 ? `${mentionChannel.join(' \n').length > 1000 ? `${mentionChannel.slice(0, 9).join(' \n')} ${message.language.get('UTILS').MORE_SIZE(mentionChannel.length - 9)}` : mentionChannel.join(' \n')}` : message.language.get('ANTICAPS')[17]}`)
+            .setTitle(message.language.get('BADWORDS')[12])
+            .addField(message.language.get('BADWORDS')[13], message.language.get('SANCTION')[data.sanction - 1])
+            .addField(message.language.get('BADWORDS')[14], data.words.length > 0 ? '•' + data.words.join(' \n•') : message.language.get('BADWORDS')[13])
+            .addField(message.language.get('BADWORDS')[16], `${mentionRole.length > 0 ? `${mentionRole.join(' \n').length > 1000 ? `${mentionRole.slice(0, 9).join(' \n')} ${message.language.get('UTILS').MORE_SIZE(mentionRole.length - 9)}` : mentionRole.join(' \n')}` : message.language.get('ANTICAPS')[17]}`)
+            .addField(message.language.get('BADWORDS')[18], `${mentionChannel.length > 0 ? `${mentionChannel.join(' \n').length > 1000 ? `${mentionChannel.slice(0, 9).join(' \n')} ${message.language.get('UTILS').MORE_SIZE(mentionChannel.length - 9)}` : mentionChannel.join(' \n')}` : message.language.get('ANTICAPS')[19]}`)
             .setFooter(this.client.user.username, this.client.user.avatarURL())
 
           message.channel.send(embedSetup)
