3343f0b590269183a52edd3ce38f18dddd74e500
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-07 22:00:44 +0200

    Update prefix.js

--- src/commands/Administration/prefix.js
+++ src/commands/Administration/prefix.js
@@ -16,24 +16,24 @@ module.exports = class Prefix extends Command {
       permission: ['ADMINISTRATOR'],
       botpermissions: ['EMBED_LINKS'],
       usage: (language, prefix) => language.get('PREFIX_USAGE', prefix),
-      category: (language) => language.get('UTILS').GUILDADMIN_CATEGORIE,
+      category: (language) => language.get('UTILS').GUILDADMIN_CATEGORY,
       examples: (language, prefix) => language.get('PREFIX_EXEMPLE', prefix)
     })
   }
 
   run (message, args) {
-    if (!args[0]) return message.channel.send(message.language.get('PREFIX_ARGS'))
+    if (!args[0]) return message.channel.send(message.language.get('PREFIX')[0])
 
-    if (args[0].length > 3) return message.channel.send(message.language.get('PREFIX_LENGTH'))
+    if (args[0].length > 3) return message.channel.send(message.language.get('PREFIX')[1])
 
-    if(args[0].match(new RegExp(/[^A-Za-z!?;:*\-+=$/@]/))) return message.channel.send(message.language.get('PREFIX_ASCII'))
+    if(args[0].match(/[^A-Za-z!?;:*\-+=$/@]/gi)) return message.channel.send(message.language.get('PREFIX')[2])
 
     this.client.database.query('UPDATE settings SET prefix = $1 WHERE id = $2', [args[0], message.guild.id])
 
     const embed = new MessageEmbed()
       .setColor(this.client.config.embed.color)
       .setTimestamp()
-      .setTitle(message.language.get('PREFIX_CHANGE'))
+      .setTitle(message.language.get('PREFIX')[3])
       .setDescription(message.language.get('PREFIX_CHANGE_DESC', args))
       .setFooter(this.client.user.username, this.client.user.avatarURL())
 
