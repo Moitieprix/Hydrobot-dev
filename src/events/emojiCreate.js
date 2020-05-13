d5237db950d88a27625053a211c75ec90131288b
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-14 00:07:46 +0200

    Update emojiCreate.js

--- src/events/emojiCreate.js
+++ src/events/emojiCreate.js
@@ -1,18 +1,16 @@
 'use strict'
 
-const {MessageEmbed} = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = async (client, guildEmoji) => {
-
-  client.database.query('SELECT * FROM settings WHERE id = $1', [guildEmoji.guild.id], async (err, res) => {
-
+  client.database.query('SELECT * FROM settings WHERE id = $1', [guildEmoji.guild.id], async (_err, res) => {
     const channel = res.rows[0].channels.logs
 
-    if (!res.rows[0].system.logs || !res.rows[0]['logs_list']['emojiCreate'] || channel === '0') return
+    if (!res.rows[0].system.logs || !res.rows[0].logs_list.emojiCreate || channel === '0') return
     if (!guildEmoji.guild.channels.cache.some(ch => ch.id === channel)) return
     if (!client.channels.cache.get(channel).permissionsFor(client.user.id).has('SEND_MESSAGES')) return
 
-    const language = new (require(`../../i18n/${res.rows[0].language}`))
+    const language = new (require(`../../i18n/${res.rows[0].language}`))()
 
     const embed = new MessageEmbed()
       .setColor(client.config.embed.color)
@@ -25,6 +23,4 @@ module.exports = async (client, guildEmoji) => {
 
     return guildEmoji.guild.channels.cache.get(channel).send(embed)
   })
-
-
 }
