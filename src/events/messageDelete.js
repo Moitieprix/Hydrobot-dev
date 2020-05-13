8e15019579faf2cae9202b56cbb6df413d739e91
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-14 00:08:43 +0200

    Update messageDelete.js

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -5,15 +5,14 @@ const { MessageEmbed } = require('discord.js')
 module.exports = async (client, message) => {
   if (message.channel.type === 'dm') return
 
-  client.database.query('SELECT * FROM settings WHERE id = $1', [message.guild.id], async (err, res) => {
-
+  client.database.query('SELECT * FROM settings WHERE id = $1', [message.guild.id], async (_err, res) => {
     const channel = res.rows[0].channels.logs
 
-    if(!res.rows[0].system.logs || !res.rows[0]['logs_list']['messageDelete'] || channel === '0') return
+    if (!res.rows[0].system.logs || !res.rows[0].logs_list.messageDelete || channel === '0') return
     if (!message.guild.channels.cache.some(ch => ch.id === channel)) return
     if (!client.channels.cache.get(channel).permissionsFor(client.user.id).has('SEND_MESSAGES')) return
 
-    const language = new (require(`../../i18n/${res.rows[0].language}`))
+    const language = new (require(`../../i18n/${res.rows[0].language}`))()
 
     if (message.partial) await message.fetch()
 
@@ -27,10 +26,9 @@ module.exports = async (client, message) => {
         .addField(language.get('LOGS').MSG_DELETED[2], `<#${message.channel.id}>`)
         .addField(language.get('LOGS').MSG_DELETED[3], `${message.content}`)
         .setTimestamp()
-        .setFooter(client.user.username, client.user.avatarURL());
+        .setFooter(client.user.username, client.user.avatarURL())
 
       return channelSend.send(embed)
-
     } else {
       const embed = new MessageEmbed()
         .setColor(client.config.embed.color)
@@ -39,10 +37,9 @@ module.exports = async (client, message) => {
         .addField(language.get('LOGS').MSG_DELETED[2], `<#${message.channel.id}>`)
         .attachFiles([{ name: 'messageDelete.txt', attachment: Buffer.from(message.content, 'utf8') }])
         .setTimestamp()
-        .setFooter(client.user.username, client.user.avatarURL());
+        .setFooter(client.user.username, client.user.avatarURL())
 
       return channelSend.send(embed)
     }
-
   })
 }
