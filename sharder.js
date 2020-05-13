d66b11aa1f16f835a6929af8be62f243558a435b
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-14 00:05:39 +0200

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -1,13 +1,16 @@
 'use strict'
 
+const { readFileSync } = require('fs')
+const { parse } = require('toml')
+
 if (Number(process.version.slice(1).split('.')[0]) < 10) { throw new Error('Node 10.0.0 or higher is required. Update Node on your system.') }
 
-const {ShardingManager} = require('discord.js')
-const config = require('./config')
-const sharder = new ShardingManager('index.js', {token: config.token, totalShards: 'auto'})
+const { ShardingManager } = require('discord.js')
+const config = parse(readFileSync('./config.toml', 'utf-8'))
+const sharder = new ShardingManager('index.js', { token: config.token, totalShards: 'auto' })
 
 sharder
   .on('shardCreate', (shard) => { console.log(`Loading of the shard ${shard.id}...`) })
   .spawn().then(() => {
-  console.log(`${sharder.totalShards} shards loaded !`)
-})
+    console.log(`${sharder.totalShards} shards loaded !`)
+  })
