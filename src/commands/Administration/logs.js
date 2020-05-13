c1c87cb63dd06e6574ba8ec582f206c0c0edc05b
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-14 00:07:29 +0200

    Update logs.js

--- src/commands/Administration/logs.js
+++ src/commands/Administration/logs.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const {MessageEmbed} = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = class Logs extends Command {
   constructor (client) {
@@ -27,47 +27,51 @@ module.exports = class Logs extends Command {
       const logslist = ['channelCreate', 'channelDelete', 'channelPinsUpdate', 'channelUpdate', 'emojiCreate', 'emojiDelete', 'emojiUpdate', 'guildBanAdd', 'guildBanRemove', 'guildMemberAdd', 'guildMemberRemove', 'guildMemberUpdate', 'guildUpdate', 'inviteCreate', 'inviteDelete', 'messageDelete', 'messageDeleteBulk', 'messageUpdate', 'roleCreate', 'roleDelete', 'roleUpdate', 'userUpdate', 'voiceStateUpdate']
 
       switch (args[0]) {
-        case 'set-channel':
+        case 'set-channel': {
           const channel = this.client.functions.channelFilter(message, args[1])
           if (!channel) return message.channel.send(message.language.get('UTILS').CHANNEL_DEFAUT)
 
           this.client.database.query(`UPDATE settings SET channels = jsonb_set(channels, '{logs}', '"${channel}"') WHERE id = $1`, [message.guild.id])
           message.channel.send(message.language.get('LOGS_CHANNEL', channel))
           break
+        }
 
-        case 'list':
+        case 'list': {
           const embedList = new MessageEmbed()
             .setColor(this.client.config.embed.color)
-            .setDescription(logslist.map(log => res.rows[0]['logs_list'][log] === true ? `**${log}** : ${this.client.emote.others.on}` : `**${log}** : ${this.client.emote.others.off}`).join(' \n'))
+            .setDescription(logslist.map(log => res.rows[0].logs_list[log] === true ? `**${log}** : ${this.client.emote.others.on}` : `**${log}** : ${this.client.emote.others.off}`).join(' \n'))
             .setTimestamp()
             .setFooter(this.client.user.username, this.client.user.avatarURL())
 
           message.channel.send(embedList)
           break
+        }
 
-        case 'enable-all':
+        case 'enable-all': {
           for (const log of logslist) {
-            if (res.rows[0]['logs_list'][log] === false) this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, \'{${log}}\', \'true\') WHERE id = $1`, [message.guild.id])
+            if (res.rows[0].logs_list[log] === false) this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, '{${log}}', 'true') WHERE id = $1`, [message.guild.id])
           }
           message.channel.send(message.language.get('LOGS')[0])
           break
+        }
 
-        case 'disable-all':
+        case 'disable-all': {
           for (const log of logslist) {
-            if (res.rows[0]['logs_list'][log] === true) this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, \'{${log}}\', \'false\') WHERE id = $1`, [message.guild.id])
+            if (res.rows[0].logs_list[log] === true) this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, '{${log}}', 'false') WHERE id = $1`, [message.guild.id])
           }
           message.channel.send(message.language.get('LOGS')[1])
           break
+        }
 
-        default:
+        default: {
           if (logslist.includes(args[0])) {
             const log = logslist.find(p => p === args[0])
 
-            if (res.rows[0]['logs_list'][log] === true) {
-              this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, \'{${log}}\', \'false\') WHERE id = $1`, [message.guild.id])
+            if (res.rows[0].logs_list[log] === true) {
+              this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, '{${log}}', 'false') WHERE id = $1`, [message.guild.id])
               return message.channel.send(message.language.get('LOGS_DISABLE', log))
             } else {
-              this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, \'{${log}}\', \'true\') WHERE id = $1`, [message.guild.id])
+              this.client.database.query(`UPDATE settings SET logs_list = jsonb_set(logs_list, '{${log}}', 'true') WHERE id = $1`, [message.guild.id])
               return message.channel.send(message.language.get('LOGS_ENABLE', log))
             }
           } else {
@@ -80,6 +84,7 @@ module.exports = class Logs extends Command {
             message.channel.send(embed)
           }
           break
+        }
       }
     })
   }
