7c41c1a7281d5d87bcbd50e4462e23212f382159
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-14 23:46:51 +0200

    Update emojiCreate.js

--- src/events/emojiCreate.js
+++ src/events/emojiCreate.js
@@ -3,24 +3,25 @@
 const { MessageEmbed } = require('discord.js')
 
 module.exports = async (client, guildEmoji) => {
-  client.database.query('SELECT * FROM settings WHERE id = $1', [guildEmoji.guild.id], async (_err, res) => {
-    const channel = res.rows[0].channels.logs
+  const res = await client.functions.getDataSettings(client, guildEmoji.guild.id)
+  if (!res) return
 
-    if (!res.rows[0].system.logs || !res.rows[0].logs_list.emojiCreate || channel === '0') return
-    if (!guildEmoji.guild.channels.cache.some(ch => ch.id === channel)) return
-    if (!client.channels.cache.get(channel).permissionsFor(client.user.id).has('SEND_MESSAGES')) return
+  const channel = res.rows[0].channels.logs
 
-    const language = new (require(`../../i18n/${res.rows[0].language}`))()
+  if (!res.rows[0].system.logs || !res.rows[0].logs_list.emojiCreate || channel === '0') return
+  if (!guildEmoji.guild.channels.cache.some(ch => ch.id === channel)) return
+  if (!client.channels.cache.get(channel).permissionsFor(client.user.id).has('SEND_MESSAGES')) return
 
-    const embed = new MessageEmbed()
-      .setColor(client.config.embed.color)
-      .setTitle(language.get('LOGS').EMOJI_CREATED[0])
-      .setThumbnail(guildEmoji.url)
-      .addField(language.get('LOGS').EMOJI_CREATED[1], guildEmoji.name)
-      .addField(language.get('LOGS').EMOJI_CREATED[2], guildEmoji.id)
-      .setTimestamp()
-      .setFooter(client.user.username, client.user.avatarURL())
+  const language = new (require(`../../i18n/${res.rows[0].language}`))()
 
-    return guildEmoji.guild.channels.cache.get(channel).send(embed)
-  })
+  const embed = new MessageEmbed()
+    .setColor(client.config.embed.color)
+    .setTitle(language.get('LOGS').EMOJI_CREATED[0])
+    .setThumbnail(guildEmoji.url)
+    .addField(language.get('LOGS').EMOJI_CREATED[1], guildEmoji.name)
+    .addField(language.get('LOGS').EMOJI_CREATED[2], guildEmoji.id)
+    .setTimestamp()
+    .setFooter(client.user.username, client.user.avatarURL())
+
+  return guildEmoji.guild.channels.cache.get(channel).send(embed)
 }
