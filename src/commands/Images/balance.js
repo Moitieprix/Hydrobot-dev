b458b311439df3ef5448205c9c00c6f6ccac2ce6
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-17 00:10:07 +0200

    Update balance.js

--- src/commands/Images/balance.js
+++ src/commands/Images/balance.js
@@ -16,7 +16,7 @@ module.exports = class Balance extends Command {
       permission: [],
       botpermissions: ['ATTACH_FILES'],
       usage: (language, prefix) => language.get('BALANCE_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
       examples: (language, prefix) => language.get('BALANCE_EXEMPLE', prefix)
     })
   }
@@ -26,13 +26,13 @@ module.exports = class Balance extends Command {
 
     if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
 
-    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, image) => {
+    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, image) => {
       image.resize(256, 256)
 
-      Jimp.read('./images/balance.png', (err, avatar) => {
+      Jimp.read('./images/balance.png', (_err, avatar) => {
         avatar.resize(256, 256)
         image.composite(avatar, 0, 0)
-        image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+        image.getBuffer(Jimp.MIME_PNG, (_err, buffer) => {
           return message.channel.send({ files: [{ name: 'balance.png', attachment: buffer }] })
         })
       })
