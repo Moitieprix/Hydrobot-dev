9a1246d220482eb56fe7a13ad626b9d4a86a043f
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-17 23:32:37 +0200

    Update sha256.js

--- src/commands/Fun/sha256.js
+++ src/commands/Fun/sha256.js
@@ -17,19 +17,19 @@ module.exports = class Sha256 extends Command {
       permission: [],
       botpermissions: ['EMBED_LINKS'],
       usage: (language, prefix) => language.get('SHA256_USAGE', prefix),
-      category: (language) => language.get('UTILS').FUN_CATEGORIE,
-      examples: (language, prefix) => language.get('SHA256_EXEMPLE', prefix)
+      category: (language) => language.get('UTILS').FUN_CATEGORY,
+      examples: (language, prefix) => language.get('SHA256_EXAMPLE', prefix)
     })
   }
 
   async run (message, args) {
-    if (!args[0]) return message.channel.send(message.language.get('SHA_ARGS'))
-    if (args.join(' ').length > 1000) return message.channel.send(message.language.get('SHA_LENGTH'))
+    if (!args[0]) return message.channel.send(message.language.get('SHA')[0])
+    if (args.join(' ').length > 1000) return message.channel.send(message.language.get('SHA')[1])
 
     const embed = new MessageEmbed()
       .setColor(this.client.config.embed.color)
-      .addField(`${message.language.get('SHA_ENTREE')}`, `\`\`\`${args.join(' ')}\`\`\``)
-      .addField(`${message.language.get('SHA_SORTIE')}`, `\`\`\`${shajs('sha256').update(args.join(' ')).digest('hex')}\`\`\``)
+      .addField(`${message.language.get('SHA')[2]}`, `\`\`\`${args.join(' ')}\`\`\``)
+      .addField(`${message.language.get('SHA')[3]}`, `\`\`\`${shajs('sha256').update(args.join(' ')).digest('hex')}\`\`\``)
       .setTimestamp()
       .setFooter(this.client.user.username, this.client.user.avatarURL())
 
