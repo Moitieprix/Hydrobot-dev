dcb1a18a9517cb23d7c0021d2670070436a56c11
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-17 23:32:29 +0200

    Update roll.js

--- src/commands/Fun/roll.js
+++ src/commands/Fun/roll.js
@@ -16,8 +16,8 @@ module.exports = class Roll extends Command {
       permission: [],
       botpermissions: ['EMBED_LINKS'],
       usage: (language, prefix) => language.get('ROLL_USAGE', prefix),
-      category: (language) => language.get('UTILS').FUN_CATEGORIE,
-      examples: (language, prefix) => language.get('ROLL_USAGE', prefix)
+      category: (language) => language.get('UTILS').FUN_CATEGORY,
+      examples: (language, prefix) => language.get('ROLL_EXAMPLE', prefix)
     })
   }
 
@@ -25,7 +25,7 @@ module.exports = class Roll extends Command {
     const embed = new MessageEmbed()
       .setColor(this.client.config.embed.color)
       .setTimestamp()
-      .addField(message.language.get('ROLL_EMBED'), this.client.functions.getRandomInt(1, 6))
+      .addField(message.language.get('ROLL'), this.client.functions.getRandomInt(1, 6))
       .setFooter(this.client.user.username, this.client.user.avatarURL())
 
     return message.channel.send({ embed: embed })
