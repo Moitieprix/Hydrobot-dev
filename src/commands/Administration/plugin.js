b5a9147f4194acfb9c0a4a8b94ea2f45ec71336d
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-17 22:15:14 +0200

    Update plugin.js

--- src/commands/Administration/plugin.js
+++ src/commands/Administration/plugin.js
@@ -21,62 +21,61 @@ module.exports = class Plugin extends Command {
   }
 
   async run (message, args) {
-    this.client.database.query('SELECT * FROM settings WHERE id = $1', [message.guild.id], (err, res) => {
-      if (err) return message.channel.send(message.language.get('UTILS').DATABASE_ERROR(err))
+    const res = await this.client.functions.getDataSettings(this.client, message.guild.id, message)
+    if (!res) return
 
-      const plugins = ['logs', 'modlogs', 'autorole', 'welcome', 'goodbye', 'antilink', 'anticaps', 'badwords', 'massmentions', 'captcha', 'nsfw', 'customCommands', 'images']
+    const plugins = ['logs', 'modlogs', 'autorole', 'welcome', 'goodbye', 'antilink', 'anticaps', 'badwords', 'massmentions', 'captcha', 'nsfw', 'customCommands', 'images']
 
-      switch (args[0]) {
-        case 'list': {
-          const embedList = new MessageEmbed()
+    switch (args[0]) {
+      case 'list': {
+        const embedList = new MessageEmbed()
+          .setColor(this.client.config.embed.color)
+          .setDescription(plugins.map(plugin => res.rows[0].system[plugin] === true ? `**${plugin}** : ${this.client.emote.others.on} \n` : `**${plugin}** : ${this.client.emote.others.off} \n`).join(' \n'))
+          .setTimestamp()
+          .setFooter(this.client.user.username, this.client.user.avatarURL())
+
+        message.channel.send(embedList)
+        break
+      }
+
+      case 'enable-all': {
+        for (const plugin of plugins) {
+          if (res.rows[0].system[plugin] === false) this.client.database.query(`UPDATE settings SET system = jsonb_set(logs_list, '{${plugin}}', 'true') WHERE id = $1`, [message.guild.id])
+        }
+        message.channel.send(message.language.get('PLUGIN')[0])
+        break
+      }
+
+      case 'disable-all': {
+        for (const plugin of plugins) {
+          if (res.rows[0].system[plugin] === true) this.client.database.query(`UPDATE settings SET system = jsonb_set(logs_list, '{${plugin}}', 'false') WHERE id = $1`, [message.guild.id])
+        }
+        message.channel.send(message.language.get('PLUGIN')[1])
+        break
+      }
+
+      default: {
+        if (plugins.includes(args[0])) {
+          const plugin = plugins.find(p => p === args[0])
+
+          if (res.rows[0].system[plugin] === true) {
+            this.client.database.query(`UPDATE settings SET system = jsonb_set(system, '{${plugin}}', 'false') WHERE id = $1`, [message.guild.id])
+            return message.channel.send(message.language.get('PLUGIN_DISABLE', plugin))
+          } else {
+            this.client.database.query(`UPDATE settings SET system = jsonb_set(system, '{${plugin}}', 'true') WHERE id = $1`, [message.guild.id])
+            return message.channel.send(message.language.get('PLUGIN_ENABLE', plugin))
+          }
+        } else {
+          const embed = new MessageEmbed()
             .setColor(this.client.config.embed.color)
-            .setDescription(plugins.map(plugin => res.rows[0].system[plugin] === true ? `**${plugin}** : ${this.client.emote.others.on} \n` : `**${plugin}** : ${this.client.emote.others.off} \n`).join(' \n'))
+            .setTitle(message.language.get('PLUGIN')[2])
+            .setDescription(message.language.get('PLUGIN')[3])
             .setTimestamp()
             .setFooter(this.client.user.username, this.client.user.avatarURL())
-
-          message.channel.send(embedList)
-          break
+          message.channel.send(embed)
         }
-
-        case 'enable-all': {
-          for (const plugin of plugins) {
-            if (res.rows[0].system[plugin] === false) this.client.database.query(`UPDATE settings SET system = jsonb_set(logs_list, '{${plugin}}', 'true') WHERE id = $1`, [message.guild.id])
-          }
-          message.channel.send(message.language.get('PLUGIN')[0])
-          break
-        }
-
-        case 'disable-all': {
-          for (const plugin of plugins) {
-            if (res.rows[0].system[plugin] === true) this.client.database.query(`UPDATE settings SET system = jsonb_set(logs_list, '{${plugin}}', 'false') WHERE id = $1`, [message.guild.id])
-          }
-          message.channel.send(message.language.get('PLUGIN')[1])
-          break
-        }
-
-        default: {
-          if (plugins.includes(args[0])) {
-            const plugin = plugins.find(p => p === args[0])
-
-            if (res.rows[0].system[plugin] === true) {
-              this.client.database.query(`UPDATE settings SET system = jsonb_set(system, '{${plugin}}', 'false') WHERE id = $1`, [message.guild.id])
-              return message.channel.send(message.language.get('PLUGIN_DISABLE', plugin))
-            } else {
-              this.client.database.query(`UPDATE settings SET system = jsonb_set(system, '{${plugin}}', 'true') WHERE id = $1`, [message.guild.id])
-              return message.channel.send(message.language.get('PLUGIN_ENABLE', plugin))
-            }
-          } else {
-            const embed = new MessageEmbed()
-              .setColor(this.client.config.embed.color)
-              .setTitle(message.language.get('PLUGIN')[2])
-              .setDescription(message.language.get('PLUGIN')[3])
-              .setTimestamp()
-              .setFooter(this.client.user.username, this.client.user.avatarURL())
-            message.channel.send(embed)
-          }
-          break
-        }
+        break
       }
-    })
+    }
   }
 }
