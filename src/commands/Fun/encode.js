068f0eccc53965bfc98e2a59cf0cedc0e275c190
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-17 22:15:27 +0200

    Update encode.js

--- src/commands/Fun/encode.js
+++ src/commands/Fun/encode.js
@@ -17,7 +17,7 @@ module.exports = class Encode extends Command {
       permission: [],
       botpermissions: ['EMBED_LINKS'],
       usage: (language, prefix) => language.get('ENCODE_USAGE', prefix),
-      category: (language) => language.get('UTILS').FUN_CATEGORIE,
+      category: (language) => language.get('UTILS').FUN_CATEGORY,
       examples: (language, prefix) => language.get('ENCODE_EXEMPLE', prefix)
     })
   }
