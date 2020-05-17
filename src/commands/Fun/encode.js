83016334ac60db5bc2bfeeda2b01d8c6b6e1a2b5
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-17 23:32:25 +0200

    Update encode.js

--- src/commands/Fun/encode.js
+++ src/commands/Fun/encode.js
@@ -23,16 +23,16 @@ module.exports = class Encode extends Command {
   }
 
   async run (message, args) {
-    if (!args[0]) return message.channel.send(message.language.get('ENCODE_ARGS'))
-    if (args.join(' ').length > 750) return message.channel.send(message.language.get('ENCODE_LENGTH'))
+    if (!args[0]) return message.channel.send(message.language.get('ENCODE')[0])
+    if (args.join(' ').length > 750) return message.channel.send(message.language.get('ENCODE')[1])
 
     const text = args.join(' ')
     const encodeText = Base64.encode(text)
 
     const embed = new MessageEmbed()
       .setColor(this.client.config.embed.color)
-      .addField(`${message.language.get('ENCODE_ENTREE')}`, `\`\`\`${text}\`\`\``)
-      .addField(`${message.language.get('ENCODE_SORTIE')}`, `\`\`\`${encodeText}\`\`\``)
+      .addField(`${message.language.get('ENCODE')[2]}`, `\`\`\`${text}\`\`\``)
+      .addField(`${message.language.get('ENCODE')[3]}`, `\`\`\`${encodeText}\`\`\``)
       .setTimestamp()
       .setFooter(this.client.user.username, this.client.user.avatarURL())
 
