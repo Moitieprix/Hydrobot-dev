c6576ce408642efe520981a207bac2f5a9a2688c
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 22:00:28 +0200

    Update posterize.js

--- src/commands/Images/posterize.js
+++ src/commands/Images/posterize.js
@@ -1,36 +1,31 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Posterize extends Command {
   constructor (client) {
     super(client, {
       name: 'posterize',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('POSTERIZE_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('POSTERIZE_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'posterize'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'posterize')
     })
   }
 
   async run (message, args) {
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, image) => {
+    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, image) => {
       image.resize(256, 256)
       image.posterize(10)
 
-      image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+      image.getBuffer(MIME_PNG, (_err, buffer) => {
         return message.channel.send({ files: [{ name: 'posterize.png', attachment: buffer }] })
       })
     })
