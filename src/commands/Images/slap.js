60066c82580f5f6d5818f540ec38787fe82d8fd1
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 22:00:48 +0200

    Update slap.js

--- src/commands/Images/slap.js
+++ src/commands/Images/slap.js
@@ -1,52 +1,45 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Slap extends Command {
   constructor (client) {
     super(client, {
       name: 'slap',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('SLAP_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('SLAP_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'slap'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'slap')
     })
   }
 
   async run (message, args) {
-    const mask = await Jimp.read('./images/mask.png')
+    const mask = await read('./images/mask.png')
 
-    if (!args[0]) {
-      return message.channel.send(message.language.get('SLAP_ARGS'))
-    }
+    if (!args[0]) return message.channel.send(message.language.get('SLAP_ARGS'))
 
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read('./images/plate_slap.png', (err, image) => {
+    read('./images/plate_slap.png', (_err, image) => {
       image.resize(512, 256)
 
-      Jimp.read(message.author.displayAvatarURL({ format: 'png', size: 256 }), (err, avatar) => {
+      read(message.author.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
         mask.resize(100, 100)
         avatar.resize(100, 100)
         avatar.mask(mask)
         image.composite(avatar, 205, 35)
 
-        Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, avatar2) => {
+        read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar2) => {
           avatar2.resize(100, 100)
           avatar2.mask(mask)
           image.composite(avatar2, 85, 90)
 
-          image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+          image.getBuffer(MIME_PNG, (_err, buffer) => {
             return message.channel.send({ files: [{ name: 'slap.png', attachment: buffer }] })
           })
         })
