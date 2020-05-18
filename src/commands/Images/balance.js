49171852f433c2498aab3dca31dd5744538e00cc
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 21:59:12 +0200

    Update balance.js

--- src/commands/Images/balance.js
+++ src/commands/Images/balance.js
@@ -1,38 +1,33 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Balance extends Command {
   constructor (client) {
     super(client, {
       name: 'balance',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('BALANCE_USAGE', prefix),
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'balance'),
       category: (language) => language.get('UTILS').IMAGE_CATEGORY,
-      examples: (language, prefix) => language.get('BALANCE_EXEMPLE', prefix)
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'balance')
     })
   }
 
   async run (message, args) {
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, image) => {
-      image.resize(256, 256)
+    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
+      avatar.resize(256, 256)
 
-      Jimp.read('./images/balance.png', (_err, avatar) => {
-        avatar.resize(256, 256)
-        image.composite(avatar, 0, 0)
-        image.getBuffer(Jimp.MIME_PNG, (_err, buffer) => {
+      read('./images/balance.png', (_err, image) => {
+        image.resize(256, 256)
+        avatar.composite(image, 0, 0)
+        avatar.getBuffer(MIME_PNG, (_err, buffer) => {
           return message.channel.send({ files: [{ name: 'balance.png', attachment: buffer }] })
         })
       })
