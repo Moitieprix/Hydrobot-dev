ceb79e3c223b64539457881b3639f31168e08217
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 21:59:15 +0200

    Update beautiful.js

--- src/commands/Images/beautiful.js
+++ src/commands/Images/beautiful.js
@@ -1,42 +1,37 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Beautiful extends Command {
   constructor (client) {
     super(client, {
       name: 'beautiful',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('BEAUTIFUL_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('BEAUTIFUL_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'beautiful'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'beautiful')
     })
   }
 
   async run (message, args) {
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read('./images/plate_beautiful.png', (err, image) => {
+    read('./images/plate_beautiful.png', (_err, image) => {
       image.resize(256, 275)
-      Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, avatar) => {
+      read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
         avatar.resize(60, 69)
         image.composite(avatar, 175, 18)
 
-        Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, avatar2) => {
+        read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar2) => {
           avatar2.resize(60, 69)
           image.composite(avatar2, 175, 155)
 
-          image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+          image.getBuffer(MIME_PNG, (_err, buffer) => {
             return message.channel.send({ files: [{ name: 'beautiful.png', attachment: buffer }] })
           })
         })
