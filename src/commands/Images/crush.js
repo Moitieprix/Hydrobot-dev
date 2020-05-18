d0c5442ab09a9763f6007db4647e1f1a53520e99
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 21:59:45 +0200

    Update crush.js

--- src/commands/Images/crush.js
+++ src/commands/Images/crush.js
@@ -1,53 +1,46 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG, BLEND_DESTINATION_OVER } = require('jimp')
 
 module.exports = class Crush extends Command {
   constructor (client) {
     super(client, {
       name: 'crush',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('CRUSH_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('CRUSH_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'crush'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'crush')
     })
   }
 
   async run (message, args) {
-    const mask = await Jimp.read('./images/mask.png')
+    const mask = await read('./images/mask.png')
 
-    if (!args[0]) {
-      return message.channel.send(message.language.get('CRUSH_ARGS'))
-    }
+    if (!args[0]) return message.channel.send(message.language.get('CRUSH_ARGS'))
 
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read(user.displayAvatarURL({format: 'png', size: 256}), (err, avatar) => {
+    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
       avatar.resize(165, 165)
       avatar.rotate(8)
 
-      Jimp.read('./images/plate_crush.png', (err, image) => {
+      read('./images/plate_crush.png', (_err, image) => {
         image.resize(256, 370)
-        image.composite(avatar, 60, 180, {mode: Jimp.BLEND_DESTINATION_OVER})
+        image.composite(avatar, 60, 180, { mode: BLEND_DESTINATION_OVER })
 
-        Jimp.read(message.author.displayAvatarURL({format: 'png', size: 256}), (err, avatar2) => {
+        read(message.author.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar2) => {
           mask.resize(50, 50)
           avatar2.resize(50, 50)
           avatar2.mask(mask)
           image.composite(avatar2, 175, 22)
 
-          image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
-            return message.channel.send({files: [{name: 'crush.png', attachment: buffer}]})
+          image.getBuffer(MIME_PNG, (_error, buffer) => {
+            return message.channel.send({ files: [{ name: 'crush.png', attachment: buffer }] })
           })
         })
       })
