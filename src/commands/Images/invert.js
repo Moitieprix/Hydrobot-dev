8045ebfd083886eb99fadd70f32135670405f8ab
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 22:00:01 +0200

    Update invert.js

--- src/commands/Images/invert.js
+++ src/commands/Images/invert.js
@@ -1,36 +1,31 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Invert extends Command {
   constructor (client) {
     super(client, {
       name: 'invert',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('INVERT_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('INVERT_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'invert'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'invert')
     })
   }
 
   async run (message, args) {
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, image) => {
-      image.resize(256, 256)
-      image.invert()
+    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
+      avatar.resize(256, 256)
+      avatar.invert()
 
-      image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+      avatar.getBuffer(MIME_PNG, (_err, buffer) => {
         return message.channel.send({ files: [{ name: 'invert.png', attachment: buffer }] })
       })
     })
