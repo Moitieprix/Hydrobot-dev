1281b7cc9ba89a090cbe6da1855c5c2d8198a1e3
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 22:01:04 +0200

    Update wasted.js

--- src/commands/Images/wasted.js
+++ src/commands/Images/wasted.js
@@ -1,41 +1,36 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Wasted extends Command {
   constructor (client) {
     super(client, {
       name: 'wasted',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('WASTED_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('WASTED_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'wasted'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'wasted')
     })
   }
 
   async run (message, args) {
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, image) => {
-      image.resize(256, 256)
-      image.greyscale()
-      image.gaussian(3)
+    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
+      avatar.resize(256, 256)
+      avatar.greyscale()
+      avatar.gaussian(3)
 
-      Jimp.read('./images/plate_wasted.png', (err, avatar) => {
-        avatar.resize(150, 150)
-        image.composite(avatar, 50, 60)
+      read('./images/plate_wasted.png', (_err, image) => {
+        image.resize(150, 150)
+        avatar.composite(image, 50, 60)
 
-        image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+        avatar.getBuffer(MIME_PNG, (_err, buffer) => {
           return message.channel.send({ files: [{ name: 'wasted.png', attachment: buffer }] })
         })
       })
