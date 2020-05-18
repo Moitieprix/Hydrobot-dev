1829306d8629e4eb465fb16dad73e49fc549dfe9
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-18 22:00:14 +0200

    Update missionpassed.js

--- src/commands/Images/missionpassed.js
+++ src/commands/Images/missionpassed.js
@@ -1,40 +1,35 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const Jimp = require('jimp')
+const { read, MIME_PNG } = require('jimp')
 
 module.exports = class Missionpassed extends Command {
   constructor (client) {
     super(client, {
       name: 'missionpassed',
       cooldown: 5,
-      enabled: true,
-      owner: false,
-      nsfw: false,
-      plugin: 'image',
-      aliases: [],
-      permission: [],
+      plugin: 'images',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('MISSIONPASSED_USAGE', prefix),
-      category: (language) => language.get('UTILS').IMAGE_CATEGORIE,
-      examples: (language, prefix) => language.get('MISSIONPASSED_EXEMPLE', prefix)
+      usage: (language, prefix) => language.get('IMAGE_USAGE', prefix, 'missionpassed'),
+      category: (language) => language.get('UTILS').IMAGE_CATEGORY,
+      examples: (language, prefix) => language.get('IMAGE_EXAMPLE', prefix, 'missionpassed')
     })
   }
 
   async run (message, args) {
     const user = await this.client.functions.userFilter(message, args)
 
-    if (!user) return message.channel.send(message.language.get('UTILS').USER_DEFAUT)
+    if (!user) return
 
-    Jimp.read(user.displayAvatarURL({ format: 'png', size: 256 }), (err, image) => {
-      image.resize(256, 256)
-      image.gaussian(3)
+    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
+      avatar.resize(256, 256)
+      avatar.gaussian(3)
 
-      Jimp.read('./images/missionpassed.png', (err, avatar) => {
-        avatar.resize(256, 185)
-        image.composite(avatar, 0, 50)
+      read('./images/missionpassed.png', (_err, image) => {
+        image.resize(256, 185)
+        avatar.composite(image, 0, 50)
 
-        image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+        avatar.getBuffer(MIME_PNG, (_err, buffer) => {
           return message.channel.send({ files: [{ name: 'missionpassed.png', attachment: buffer }] })
         })
       })
