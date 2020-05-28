1f11a04bf67970da1c3fce9d60293b5eef3317dd
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-28 15:32:54 +0200

    Update ping.js

--- src/commands/Hydrobot/ping.js
+++ src/commands/Hydrobot/ping.js
@@ -7,12 +7,6 @@ module.exports = class Ping extends Command {
   constructor (client) {
     super(client, {
       name: 'ping',
-      cooldown: 3,
-      enabled: true,
-      owner: false,
-      plugin: false,
-      aliases: [],
-      permission: [],
       botpermissions: ['EMBED_LINKS'],
       usage: (language, prefix) => language.get('PING_USAGE', prefix),
       category: (language) => language.get('UTILS').HYDROBOT_CATEGORY,
@@ -21,22 +15,20 @@ module.exports = class Ping extends Command {
   }
 
   async run (message) {
-    const loading = new MessageEmbed()
+    const msg = await message.channel.send(new MessageEmbed()
       .setColor(this.client.config.embed.color)
       .setTitle(':ping_pong: • Ping !')
       .setTimestamp()
       .setFooter(this.client.user.username, this.client.user.avatarURL())
+    )
 
-    const msg = await message.channel.send(loading)
-
-    const embed = new MessageEmbed()
+    msg.edit(new MessageEmbed()
       .setColor(this.client.config.embed.color)
       .setTitle(':ping_pong: • Pong !')
       .addField(message.language.get('PING')[0], `\`${msg.createdTimestamp - message.createdTimestamp}ms\``, true)
       .addField(message.language.get('PING')[1], '`' + Math.round(this.client.ws.ping) + 'ms`', true)
       .setTimestamp()
       .setFooter(this.client.user.username, this.client.user.avatarURL())
-
-    return msg.edit(embed)
+    )
   }
 }
