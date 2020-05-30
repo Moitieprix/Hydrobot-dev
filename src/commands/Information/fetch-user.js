42dbe175d1b4e1d204d00ea546fa545b31167046
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-30 18:31:03 +0200

    Create fetch-user.js

---
+++ src/commands/Information/fetch-user.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const Command = require('../../../core/Command.js')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class FetchUser extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'fetch-user',
+      botpermissions: ['EMBED_LINKS'],
+      usage: (language, prefix) => language.get('FETCHUSER_USAGE', prefix),
+      category: (language) => language.get('UTILS').INFORMATION_CATEGORY,
+      examples: (language, prefix) => language.get('FETCHUSER_EXAMPLE', prefix)
+    })
+  }
+
+  async run (message, args) {
+    if (!args[0] || isNaN(args[0]) || args[0].length !== 18) {
+      message.channel.send(message.language.get('FETCHUSER')[8])
+      return
+    }
+
+    try {
+      const user = await this.client.users.fetch(args[0])
+
+      const flags = await user.fetchFlags()
+      const array = Object.entries(flags.serialize())
+      const flagsArray = []
+      array.map(flag => {
+        if (flag[1]) return flagsArray.push(`\`${flag[0]}\``)
+      })
+
+      message.channel.send(new MessageEmbed()
+        .setColor(this.client.config.embed.color)
+        .setAuthor(user.username, user.displayAvatarURL({ dynamic: true }))
+        .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+        .setDescription(message.language.get('FETCHUSER')[0])
+        .addField(message.language.get('FETCHUSER')[1], `${message.language.get('FETCHUSER')[2]} **${user.tag}** \n${message.language.get('FETCHUSER')[3]} **${user.id}** \n${message.language.get('FETCHUSER')[4]} **${this.client.functions.getDate(user.createdAt.toString().split(' '), message)}** \n${message.language.get('FETCHUSER')[5]} **${message.language.get('UTILS').STATUS[user.presence.status]}**`)
+        .addField(message.language.get('FETCHUSER')[6], flagsArray ? flagsArray.join(', ') : message.language.get('FETCHUSER')[7])
+        .setTimestamp()
+        .setFooter(this.client.user.username, this.client.user.avatarURL())
+      )
+    } catch (err) {
+      message.channel.send(message.language.get('FETCHUSER')[9])
+    }
+  }
+}
