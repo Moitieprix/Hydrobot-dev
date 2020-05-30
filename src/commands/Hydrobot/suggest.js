2cd66d618bae94ada9921a56d71960e6534c3a14
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-05-30 18:30:55 +0200

    Update suggest.js

--- src/commands/Hydrobot/suggest.js
+++ src/commands/Hydrobot/suggest.js
@@ -1,12 +1,11 @@
 'use strict'
 
 const Command = require('../../../core/Command.js')
-const { MessageEmbed, WebhookClient } = require('discord.js')
 
 module.exports = class Suggest extends Command {
   constructor (client) {
     super(client, {
-      name: 'support',
+      name: 'suggest',
       botpermissions: ['EMBED_LINKS'],
       usage: (language, prefix) => language.get('SUGGEST_USAGE', prefix),
       category: (language) => language.get('UTILS').HYDROBOT_CATEGORY,
@@ -14,21 +13,29 @@ module.exports = class Suggest extends Command {
     })
   }
 
-  run (message, args) {
+  async run (message, args) {
     if (!args[0]) {
       message.channel.send(message.language.get('SUGGEST')[0])
       return
     }
 
-    new WebhookClient(this.client.config.webhooks.suggestions.id, this.client.config.webhooks.suggestions.token).send(
-      new MessageEmbed()
-        .setColor(this.client.config.embed.color)
-        .setDescription(message.language.get('SUPPORT'))
+    this.client.shard.broadcastEval(`
+      const channel = this.channels.cache.get('669983836389507122')
+      if (channel) {
+        const { MessageEmbed } = require('discord.js')
+
+        channel.send(new MessageEmbed()
+        .setColor('${this.client.config.embed.color}')
+        .setTitle(\`${message.author.tag}'s suggestion, sent from ${message.guild.name} guild !\`)
+        .setDescription('${args.join(' ')}')
         .setTimestamp()
-        .setFooter(this.client.user.username, this.client.user.avatarURL())
-    ).then(async msg => {
-      //const yes = await this.client.e
-      msg.react()
-    })
+        .setFooter('${this.client.user.username}', '${this.client.user.avatarURL()}')
+      ).then(async msg => {
+        await msg.react('704689894571900988')
+        await msg.react('➖')
+        await msg.react('704689891250012201')
+      })
+      }
+    `).then(message.channel.send(message.language.get('SUGGEST')[1]))
   }
 }
