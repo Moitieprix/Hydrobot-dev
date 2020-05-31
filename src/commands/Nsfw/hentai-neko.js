854c21b6b7b53aa6b0992d1d06e0df61fa24b665
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-01 00:22:55 +0200

    Create hentai-neko.js

---
+++ src/commands/Nsfw/hentai-neko.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Command = require('../../../core/Command.js')
+const fetch = require('node-fetch')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class Hneko extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'hentai-neko',
+      cooldown: 10,
+      plugin: 'nsfw',
+      botpermissions: ['ATTACH_FILES'],
+      usage: (language, prefix) => language.get('NSFW_USAGE', prefix, 'hentai-neko'),
+      category: (language) => language.get('UTILS').NSFW_CATEGORY,
+      examples: (language, prefix) => language.get('NSFW_EXEMPLE', prefix, 'hentai-neko')
+    })
+  }
+
+  async run (message) {
+    const res = await fetch('https://nekobot.xyz/api/image?type=hneko')
+    const body = await res.json()
+
+    message.channel.send(new MessageEmbed()
+      .setColor(this.client.config.embed.color)
+      .setDescription(message.language.get('IMAGE_NOT_DISPLAYED', body.message))
+      .setImage(body.message)
+      .setTimestamp()
+      .setFooter(this.client.user.username, this.client.user.avatarURL())
+    )
+  }
+}
