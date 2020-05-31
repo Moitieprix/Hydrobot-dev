c53670b4f772979ef4c825c71e234ce438db815b
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-01 00:23:24 +0200

    Update pussy.js

---
+++ src/commands/Nsfw/pussy.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Command = require('../../../core/Command.js')
+const fetch = require('node-fetch')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class Pussy extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'pussy',
+      cooldown: 10,
+      plugin: 'nsfw',
+      botpermissions: ['ATTACH_FILES'],
+      usage: (language, prefix) => language.get('NSFW_USAGE', prefix, 'pussy'),
+      category: (language) => language.get('UTILS').NSFW_CATEGORY,
+      examples: (language, prefix) => language.get('NSFW_EXEMPLE', prefix, 'pussy')
+    })
+  }
+
+  async run (message) {
+    const res = await fetch('https://nekobot.xyz/api/image?type=pussy')
+    const body = await res.json()
+
+    message.channel.send(new MessageEmbed()
+      .setColor(this.client.config.embed.color)
+      .setDescription(message.language.get('IMAGE_NOT_DISPLAYED', body.message))
+      .setImage(body.message)
+      .setTimestamp()
+      .setFooter(this.client.user.username, this.client.user.avatarURL())
+    )
+  }
+}
