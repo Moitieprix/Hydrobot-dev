5b57784278d6a9fe21ea6582320cfaf5f78b66e7
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-01 00:22:27 +0200

    Update 4k.js

--- src/commands/Nsfw/4k.js
+++ src/commands/Nsfw/4k.js
@@ -2,35 +2,31 @@
 
 const Command = require('../../../core/Command.js')
 const fetch = require('node-fetch')
-const {MessageEmbed} = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = class Fourk extends Command {
   constructor (client) {
     super(client, {
       name: '4k',
-      cooldown: 10 ,
+      cooldown: 10,
       plugin: 'nsfw',
       botpermissions: ['ATTACH_FILES'],
-      usage: (language, prefix) => language.get('BALANCE_USAGE', prefix),
+      usage: (language, prefix) => language.get('NSFW_USAGE', prefix, '4k'),
       category: (language) => language.get('UTILS').NSFW_CATEGORY,
-      examples: (language, prefix) => language.get('BALANCE_EXEMPLE', prefix)
+      examples: (language, prefix) => language.get('NSFW_EXEMPLE', prefix, '4k')
     })
   }
 
   async run (message) {
-    fetch('https://nekobot.xyz/api/image?type=4k')
-      .then(res => res.json())
-      .then(res => {
-        const embed = new MessageEmbed()
-          .setColor(this.client.config.embed.color)
-          .setImage(res.message)
-          .setTimestamp()
-          .setFooter(this.client.user.username, this.client.user.avatarURL())
-        return message.channel.send(embed)
-      })
-      .catch(err => {
-        return message.channel.send(message.language.get('UTILS').API_ERROR(err))
-      })
+    const res = await fetch('https://nekobot.xyz/api/image?type=4k')
+    const body = await res.json()
 
+    message.channel.send(new MessageEmbed()
+      .setColor(this.client.config.embed.color)
+      .setDescription(message.language.get('IMAGE_NOT_DISPLAYED', body.message))
+      .setImage(body.message)
+      .setTimestamp()
+      .setFooter(this.client.user.username, this.client.user.avatarURL())
+    )
   }
 }
