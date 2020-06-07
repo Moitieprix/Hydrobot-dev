211abc8d9c6711d095a8faad3b75eb6b43644f08
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-07 23:45:42 +0200

    Update autorole.js

--- src/commands/Administration/autorole.js
+++ src/commands/Administration/autorole.js
@@ -16,10 +16,7 @@ module.exports = class Autorole extends Command {
     })
   }
 
-  async run (message, args) {
-    const res = await this.client.functions.getDataSettings(this.client, message.guild.id, message)
-    if (!res) return
-
+  async run (message, args, res) {
     const data = res.rows[0].autorole
 
     switch (args[0]) {
