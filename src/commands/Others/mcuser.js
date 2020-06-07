57bc7a3a8276c9e77ff02a57a7ffed4f38067d58
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-07 23:46:09 +0200

    Update mcuser.js

---
+++ src/commands/Others/mcuser.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const Command = require('../../../core/Command.js')
+const fetch = require('node-fetch')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class Mcuser extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'mcuser',
+      aliases: ['minecraftuser', 'minecraft-user', 'mc-user'],
+      botpermissions: ['EMBED_LINKS'],
+      usage: (language, prefix) => language.get('MCUSER_USAGE', prefix),
+      category: (language) => language.get('UTILS').OTHERS_CATEGORY,
+      examples: (language, prefix) => language.get('MCUSER_EXAMPLE', prefix)
+    })
+  }
+
+  async run (message, args) {
+    if (!args[0]) {
+      message.channel.send(message.language.get('MCUSER')[0])
+      return
+    }
+
+    try {
+      const resUuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`)
+      const bodyUuid = await resUuid.json()
+
+      const resHistory = await fetch(`https://api.mojang.com/user/profiles/${bodyUuid.id}/names`)
+      const bodyHistory = await resHistory.json()
+
+      const UUID = bodyUuid.id.match(/(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/)
+      UUID.shift()
+
+      const pseudoHistory = bodyHistory.map(element => {
+        return element.changedToAt ? `${element.name} • ${this.client.functions.getDate(new Date(element.changedToAt).toString().split(' '), message)}` : element.name
+      })
+
+      pseudoHistory.reverse()
+
+      message.channel.send(new MessageEmbed()
+        .setColor(this.client.config.embed.color)
+        .setAuthor('Minecraft', 'https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/iso-grassblock.png', 'https://www.minecraft.net')
+        .setTitle(`:desktop: • ${message.language.get('MCUSER')[1]}`)
+        .addField(`:label: • ${message.language.get('MCUSER')[2]}`, args[0])
+        .addField(`:credit_card: • ${message.language.get('MCUSER')[3]}`, UUID.join('-'))
+        .addField(`:credit_card: • ${message.language.get('MCUSER')[4]}`, bodyUuid.id)
+        .addField(`:film_frames: • ${message.language.get('MCUSER')[5]}`, pseudoHistory.join(' \n'))
+        .addField(`:busts_in_silhouette: • ${message.language.get('MCUSER')[6]}`, message.language.get('MCUSER_SKIN', `https://minotar.net/skin/${bodyUuid.id}`))
+        .setThumbnail(`https://minotar.net/armor/body/${bodyUuid.id}/256.png`)
+        .setTimestamp()
+        .setFooter(this.client.user.username, this.client.user.avatarURL())
+      )
+    } catch (e) {
+      message.channel.send(message.language.get('MCUSER')[7])
+    }
+  }
+}
