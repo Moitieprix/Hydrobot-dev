f0240011da87df05309b5818a5796cceae8e8e39
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-08 17:56:26 +0200

    Update messageUpdate.js

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -23,25 +23,25 @@ module.exports = async (client, oldMessage, newMessage) => {
       if (oldMessage.content.length < 1023 || newMessage.content.length < 1023) {
         const embed = new MessageEmbed()
           .setColor(client.config.embed.color)
-          .setTitle(language.get('LOGS').MSG_UPDATED[0])
-          .setDescription(`[[${language.get('LOGS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
-          .addField(language.get('LOGS').MSG_UPDATED[1], `${oldMessage.author.tag} (ID: ${oldMessage.author.id})`)
-          .addField(language.get('LOGS').MSG_UPDATED[2], `<#${oldMessage.channel.id}>`)
-          .addField(language.get('LOGS').MSG_UPDATED[3], `${oldMessage.content}`)
-          .addField(language.get('LOGS').MSG_UPDATED[4], `${newMessage.content}`)
+          .setTitle(language.get('LOGS_EVENTS').MSG_UPDATED[0])
+          .setDescription(`[[${language.get('LOGS_EVENTS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
+          .addField(language.get('LOGS_EVENTS').MSG_UPDATED[1], `${oldMessage.author.tag} (ID: ${oldMessage.author.id})`)
+          .addField(language.get('LOGS_EVENTS').MSG_UPDATED[2], `<#${oldMessage.channel.id}>`)
+          .addField(language.get('LOGS_EVENTS').MSG_UPDATED[3], `${oldMessage.content}`)
+          .addField(language.get('LOGS_EVENTS').MSG_UPDATED[4], `${newMessage.content}`)
           .setTimestamp()
           .setFooter(client.user.username, client.user.avatarURL())
 
         channelSend.send(embed)
       } else {
-        const fileContent = `${language.get('LOGS').MSG_UPDATED[3]} \n${oldMessage.content} \n\n\n${language.get('LOGS').MSG_UPDATED[4]} \n${newMessage.content}`
+        const fileContent = `${language.get('LOGS_EVENTS').MSG_UPDATED[3]} \n${oldMessage.content} \n\n\n${language.get('LOGS').MSG_UPDATED[4]} \n${newMessage.content}`
 
         const embed = new MessageEmbed()
           .setColor(client.config.embed.color)
-          .setTitle(language.get('LOGS').MSG_UPDATED[0])
-          .setDescription(`[[${language.get('LOGS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
-          .addField(language.get('LOGS').MSG_UPDATED[1], `${oldMessage.author.tag} (ID: ${oldMessage.author.id})`)
-          .addField(language.get('LOGS').MSG_UPDATED[2], `<#${oldMessage.channel.id}>`)
+          .setTitle(language.get('LOGS_EVENTS').MSG_UPDATED[0])
+          .setDescription(`[[${language.get('LOGS_EVENTS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
+          .addField(language.get('LOGS_EVENTS').MSG_UPDATED[1], `${oldMessage.author.tag} (ID: ${oldMessage.author.id})`)
+          .addField(language.get('LOGS_EVENTS').MSG_UPDATED[2], `<#${oldMessage.channel.id}>`)
           .attachFiles([{ name: 'messageUpdate.txt', attachment: Buffer.from(fileContent, 'utf8') }])
           .setTimestamp()
           .setFooter(client.user.username, client.user.avatarURL())
@@ -51,9 +51,9 @@ module.exports = async (client, oldMessage, newMessage) => {
     } else if (!oldMessage.pinned && newMessage.pinned) {
       const embed = new MessageEmbed()
         .setColor(client.config.embed.color)
-        .setTitle(language.get('LOGS').MSG_UPDATED[6])
-        .setDescription(`[[${language.get('LOGS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
-        .addField(language.get('LOGS').MSG_UPDATED[8], `<#${oldMessage.channel.id}>`)
+        .setTitle(language.get('LOGS_EVENTS').MSG_UPDATED[6])
+        .setDescription(`[[${language.get('LOGS_EVENTS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
+        .addField(language.get('LOGS_EVENTS').MSG_UPDATED[8], `<#${oldMessage.channel.id}>`)
         .setTimestamp()
         .setFooter(client.user.username, client.user.avatarURL())
 
@@ -61,9 +61,9 @@ module.exports = async (client, oldMessage, newMessage) => {
     } else if (oldMessage.pinned && !newMessage.pinned) {
       const embed = new MessageEmbed()
         .setColor(client.config.embed.color)
-        .setTitle(language.get('LOGS').MSG_UPDATED[7])
-        .setDescription(`[[${language.get('LOGS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
-        .addField(language.get('LOGS').MSG_UPDATED[9], `<#${oldMessage.channel.id}>`)
+        .setTitle(language.get('LOGS_EVENTS').MSG_UPDATED[7])
+        .setDescription(`[[${language.get('LOGS_EVENTS').MSG_UPDATED[5]}]](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`)
+        .addField(language.get('LOGS_EVENTS').MSG_UPDATED[9], `<#${oldMessage.channel.id}>`)
         .setTimestamp()
         .setFooter(client.user.username, client.user.avatarURL())
 
