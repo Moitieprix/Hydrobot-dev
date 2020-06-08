bd654ed3f1941cf148054d6e4003516d8834af26
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-08 17:56:04 +0200

    Update npm.js

---
+++ src/commands/Others/npm.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const Command = require('../../../core/Command.js')
+const fetch = require('node-fetch')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class Npm extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'npm',
+      botpermissions: ['EMBED_LINKS'],
+      usage: (language, prefix) => language.get('NPM_USAGE', prefix),
+      category: (language) => language.get('UTILS').OTHERS_CATEGORY,
+      examples: (language, prefix) => language.get('NPM_EXAMPLE', prefix)
+    })
+  }
+
+  async run (message, args) {
+    if (!args[0]) {
+      message.channel.send(message.language.get('NPM')[0])
+      return
+    }
+
+    try {
+      const res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${encodeURIComponent(args.join(' '))}&size=1`)
+      const body = await res.json()
+
+      if (!body.objects.length) {
+        message.channel.send(message.language.get('NPM')[1])
+        return
+      }
+
+      const resDownload = await fetch(`https://api.npmjs.org/downloads/point/last-week/${body.objects[0].package.name}`)
+      const bodyDownload = await resDownload.json()
+
+      message.channel.send(new MessageEmbed()
+        .setColor(this.client.config.embed.color)
+        .setAuthor('NPM', 'https://i.imgur.com/ErKf5Y0.png', 'https://www.npmjs.com/')
+        .setTitle(`:label: • ${body.objects[0].package.name}`)
+        .setDescription(body.objects[0].package.description ? body.objects[0].package.description : message.language.get('NPM')[2])
+        .addField(message.language.get('NPM')[3], body.objects[0].package.version)
+        .addField(message.language.get('NPM')[4], this.client.functions.getDate(new Date(body.objects[0].package.date).toString().split(' '), message))
+        .addField(message.language.get('NPM')[5], body.objects[0].package.author.name)
+        .addField(message.language.get('NPM')[6], body.objects[0].package.maintainers.map(element => element.username).join(', '))
+        .addField(message.language.get('NPM')[7], bodyDownload.downloads)
+        .addField(message.language.get('NPM')[8], body.objects[0].package.keywords.join(', '))
+        .addField(message.language.get('NPM')[9], `[[${message.language.get('NPM')[10]}]](${body.objects[0].package.links.npm}) • [[${message.language.get('NPM')[11]}]](${body.objects[0].package.links.homepage}) • [[${message.language.get('NPM')[12]}]](${body.objects[0].package.links.repository})`)
+        .setTimestamp()
+        .setFooter(this.client.user.username, this.client.user.avatarURL())
+      )
+    } catch {
+      message.channel.send(message.language.get('NPM')[13])
+    }
+  }
+}
