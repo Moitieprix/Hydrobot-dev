443106c8f719af05d107ac2219044d3b75b16813
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-12 23:22:04 +0200

    Update achievement.js

--- src/commands/Images/achievement.js
+++ src/commands/Images/achievement.js
@@ -16,20 +16,23 @@ module.exports = class Achievement extends Command {
   }
 
   run (message, args) {
-    const number = this.client.functions.getRandomInt(1, 39)
+    try {
+      const number = this.client.functions.getRandomInt(1, 39)
 
-    if (!args[0]) return message.channel.send(message.language.get('ACHIEVEMENT')[0])
+      if (!args[0]) return message.channel.send(message.language.get('ACHIEVEMENT')[0])
 
-    if (args.join(' ').length > 24) return message.channel.send(message.language.get('ACHIEVEMENT')[1])
+      if (args.join(' ').length > 24) return message.channel.send(message.language.get('ACHIEVEMENT')[1])
 
-    const text = encodeURIComponent(args.join(' ')).replace('%20', '+')
+      const text = encodeURIComponent(args.join(' ')).replace('%20', '+')
 
-    return message.channel.send({
-      files: [{
-        name: 'achievement.png',
-        attachment: `https://minecraftskinstealer.com/achievement/${number}/Achievement+Get%21/${text}`
-      }]
-
-    })
+      return message.channel.send({
+        files: [{
+          name: 'achievement.png',
+          attachment: `https://minecraftskinstealer.com/achievement/${number}/Achievement+Get%21/${text}`
+        }]
+      })
+    } catch (e) {
+      message.channel.send(message.language.get('ERRORS').IMAGE_ERROR(e))
+    }
   }
 }
