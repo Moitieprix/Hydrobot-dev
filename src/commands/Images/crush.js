cbf4ce8bd9f8f4238d68baf2ab5f04a4ec9e4dde
Moitieprix <42463541+Moitieprix@users.noreply.github.com>
2020-06-12 23:22:21 +0200

    Update crush.js

--- src/commands/Images/crush.js
+++ src/commands/Images/crush.js
@@ -17,33 +17,41 @@ module.exports = class Crush extends Command {
   }
 
   async run (message, args) {
-    const mask = await read('./images/mask.png')
-
-    if (!args[0]) return message.channel.send(message.language.get('CRUSH_ARGS'))
+    if (!args[0]) {
+      message.channel.send(message.language.get('CRUSH_ARGS'))
+      return
+    }
 
     const user = await this.client.functions.userFilter(message, args)
 
     if (!user) return
 
-    read(user.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar) => {
-      avatar.resize(165, 165)
-      avatar.rotate(8)
+    try {
+      const mask = await read('./images/mask.png')
+      const avatarAuthor = await read(message.author.displayAvatarURL({ format: 'png', size: 256 }))
+      const avatarMentionned = await read(user.displayAvatarURL({ format: 'png', size: 256 }))
+      const template = await read('./images/plate_crush.png')
 
-      read('./images/plate_crush.png', (_err, image) => {
-        image.resize(256, 370)
-        image.composite(avatar, 60, 180, { mode: BLEND_DESTINATION_OVER })
+      avatarAuthor.resize(50, 50)
+      avatarMentionned.resize(165, 165)
+      template.resize(256, 370)
+      mask.resize(50, 50)
 
-        read(message.author.displayAvatarURL({ format: 'png', size: 256 }), (_err, avatar2) => {
-          mask.resize(50, 50)
-          avatar2.resize(50, 50)
-          avatar2.mask(mask)
-          image.composite(avatar2, 175, 22)
+      avatarMentionned.rotate(8)
+      template.composite(avatarMentionned, 60, 180, { mode: BLEND_DESTINATION_OVER })
+      avatarAuthor.mask(mask)
+      avatarMentionned.composite(avatarAuthor, 175, 22)
 
-          image.getBuffer(MIME_PNG, (_error, buffer) => {
-            return message.channel.send({ files: [{ name: 'crush.png', attachment: buffer }] })
-          })
-        })
+      const buffer = await template.getAsyncBuffer(MIME_PNG)
+
+      message.channel.send({
+        files: [{
+          name: 'crush.png',
+          attachment: buffer
+        }]
       })
-    })
+    } catch (e) {
+      message.channel.send(message.language.get('ERRORS').IMAGE_ERROR(e))
+    }
   }
 }
